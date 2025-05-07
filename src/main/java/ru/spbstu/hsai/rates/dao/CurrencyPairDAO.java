0b6ebaf8fdd985c77f19a544a76ec7a8c45b570e
ArseniyBogdan <bogdan.senya@mail.ru>
2025-05-07 14:38:46 +0300

    Забыл закоммитить файл. Поменял тип ID для CurrencyPairDAO

--- src/main/java/ru/spbstu/hsai/rates/dao/CurrencyPairDAO.java
+++ src/main/java/ru/spbstu/hsai/rates/dao/CurrencyPairDAO.java
@@ -1,9 +1,10 @@
 package ru.spbstu.hsai.rates.dao;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.spbstu.hsai.rates.entities.CurrencyPairDBO;
 
 @Repository
-public interface CurrencyPairDAO extends ReactiveCrudRepository<CurrencyPairDBO, Long> {
+public interface CurrencyPairDAO extends ReactiveCrudRepository<CurrencyPairDBO, ObjectId> {
 }

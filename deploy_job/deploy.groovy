ed74454ef8f54073fff17021880930998148dc4e
ArseniyBogdan <bogdan.senya@mail.ru>
2026-03-08 15:43:31 +0300

    edit ssh key

--- deploy_job/deploy.groovy
+++ deploy_job/deploy.groovy
@@ -9,7 +9,7 @@ pipeline {
         DOCKER_REPO = 'arseniybogdan/currency-converter-bot'
         
         // Имя credentials в Jenkins (SSH Username with private key)
-        SSH_KEY_NAME = 'ch_opstack_ssh_key'
+        SSH_KEY_NAME = 'Arseniy'
 
         INFRA_ARTIFACT_JOB = 'Bogdan/create-infra'
         BUILD_ARTIFACT_JOB = 'Bogdan/build'

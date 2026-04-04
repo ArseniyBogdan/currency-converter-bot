ca8ea3c4084f5543b49559cbefce7128902433e5
ArseniyBogdan <bogdan.senya@mail.ru>
2026-04-04 20:12:29 +0300

    bugfix

--- infra_job_2/main.tf
+++ infra_job_2/main.tf
@@ -37,7 +37,7 @@ variable "image_family" {
 variable "security_group_id" {
   type        = string
   description = "security group id"
-  default     = "enpv49erji0f40prfu72"
+  default     = "enpkd9np0qbhc064o9mu"
 }
 
 data "yandex_vpc_subnet" "main" {

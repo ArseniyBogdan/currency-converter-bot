337eff10bfb592f6022a2747f75f0bacbd99a15b
ArseniyBogdan <bogdan.senya@mail.ru>
2026-04-04 19:22:40 +0300

    bugfix

--- infra_job_2/main.tf
+++ infra_job_2/main.tf
@@ -6,16 +6,6 @@ terraform {
     }
   }
   required_version = ">= 1.0"
-
-  provider_installation {
-    filesystem_mirror {
-      path    = "/tmp/terraform-providers"
-      include = ["yandex-cloud/yandex"]
-    }
-    direct {
-      exclude = ["yandex-cloud/yandex"]
-    }
-  }
 }
 
 provider "yandex" {

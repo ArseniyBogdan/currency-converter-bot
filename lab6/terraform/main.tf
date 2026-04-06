a80496d24b91129956e6581715487cdc11117e89
kseniasklarova <k.shklyarova30@mail.ru>
2026-04-06 13:26:56 +0300

    обновила main.tf конфликт сети был

--- lab6/terraform/main.tf
+++ lab6/terraform/main.tf
@@ -7,7 +7,7 @@ resource "yandex_vpc_subnet" "default" {
   name           = "shklyarova-bot-vm-subnet"
   zone           = "ru-central1-d"
   network_id     = "enpq1korg6qpq5kr687c"  # ← Существующая сеть default
-  v4_cidr_blocks = ["192.168.100.0/24"]     # ← Уникальная подсеть для вас
+  v4_cidr_blocks = ["192.168.101.0/24"]     # ← Уникальная подсеть для вас
 }
 
 

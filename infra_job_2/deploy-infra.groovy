58a800dfa2ca5be0bb898cba02c127383b4ec58b
ArseniyBogdan <bogdan.senya@mail.ru>
2026-04-09 19:43:27 +0300

    bugfix

--- infra_job_2/deploy-infra.groovy
+++ infra_job_2/deploy-infra.groovy
@@ -3,6 +3,7 @@ pipeline {
 
     // Привязка секретов из Jenkins Credentials
     environment {
+        YC_TOKEN              = credentials('yc-token')
         TF_FOLDER_ID          = "b1gm94s1sde2ispi5k21"
         TF_SUBNET_ID          = "fl80id702e4irnblcd63"
         TF_SSH_PUB_KEY        = credentials('ssh-public-key')
@@ -14,14 +15,6 @@ pipeline {
     }
 
     stages {
-
-        stage('Preperation'){
-            steps {
-                withCredentials([string(credentialsId: 'oauth-token', variable: 'TOKEN')]) {
-                    sh 'yc config set token ${TOKEN}'
-                }
-            }
-        }
 
         stage('Terraform: Init & Plan') {
             steps {
